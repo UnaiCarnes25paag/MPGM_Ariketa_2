257f2425f0abe16ab4133e82047b631eac04be2c
UnaiCarnes25paag <ucarnesat25paag@ikzubirimanteo.com>
2025-09-11 13:55:28 +0200

    BMI Goth Calculator Final form

--- app/src/main/java/com/example/ariketa2/MainActivity.java
+++ app/src/main/java/com/example/ariketa2/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ariketa2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +14,90 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultText;
+
+    private Button calculateButton;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private EditText ageEditText;
+    private EditText feetEditText;
+    private EditText inchesEditText;
+    private EditText weightEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        findViews();
+
+        setupButtonClickListener();
+    }
+    private void findViews(){
+        resultText = findViewById(R.id.text_view_results);
+
+        maleButton = findViewById(R.id.radio_button_male);
+        femaleButton = findViewById(R.id.radio_button_female);
+
+        ageEditText = findViewById(R.id.edit_text_age);
+        feetEditText = findViewById(R.id.edit_text_feet);
+        inchesEditText = findViewById(R.id.edit_text_inches);
+        weightEditText = findViewById(R.id.edit_text_weight);
+
+        calculateButton = findViewById(R.id.edit_button_calculate);
+    }
+
+    private void setupButtonClickListener() {
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double bmiResult = calculateBMI();
+
+                String ageText = ageEditText.getText().toString();
+                int age = Integer.parseInt(ageText);
+
+                if (age < 18) {
+                    System.out.println("Not legal Goth");
+                } else {
+                    displayResult(bmiResult);
+                }
+            }
+        });
+    }
+
+    private double calculateBMI(){
+        String feetText = feetEditText.getText().toString();
+        String inchesText = inchesEditText.getText().toString();
+        String weightText = weightEditText.getText().toString();
+
+        int feet = Integer.parseInt(feetText);
+        int inches = Integer.parseInt(inchesText);
+        int weight = Integer.parseInt(weightText);
+
+        int totalInches = (feet * 12) + inches;
+        double heightInMeter = totalInches * 0.0254;
+        return weight / (heightInMeter * heightInMeter);
+    }
+
+    private void displayResult(double bmi){
+
+        DecimalFormat myDecimalFormat = new DecimalFormat("0.00");
+        String bmiTextResult = myDecimalFormat.format(bmi);
+
+        String fullResultString;
+
+        if (bmi < 18.5){
+            fullResultString = bmiTextResult + " Slim goth";
+        } else if (bmi > 25){
+            fullResultString = bmiTextResult + " Gothicc";
+        } else {
+            fullResultString = bmiTextResult + " Goth";
+        }
+
+        resultText.setText(fullResultString);
     }
 }
